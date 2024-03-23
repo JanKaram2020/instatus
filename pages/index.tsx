6cbeb29ebdf30bab92bcd50245fd3b0a2731979f
Jan Karam <jghaly@shory.com>
2024-03-23 18:20:50 +0200

    move font to _app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
-import { Inter } from "next/font/google";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { FormattedEvent } from "@/lib/format-event";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(
   data: InferGetServerSidePropsType<typeof getServerSideProps>,
 ) {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       <pre className={"overflow-x-scroll max-w-full break-words"}>
         {JSON.stringify(data, null, 2)}
@@ -23,7 +20,6 @@ export const getServerSideProps = (async () => {
     const eventsRes = await fetch("http://localhost:3000/api/events");
     const data: { events: FormattedEvent[]; length: number } =
       await eventsRes.json();
-    console.log(data);
     return { props: data };
   } catch (e) {
     return { props: { events: null, length: null } };
