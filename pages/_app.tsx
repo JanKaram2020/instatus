65f588922fae8867b9a7bf04321574a4fd0d1d60
Jan Karam <jghaly@shory.com>
2024-03-25 00:01:59 +0200

    add next progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
@@ -7,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={inter.className}>
+      <NextNProgress />
       <Component {...pageProps} />
     </main>
   );
